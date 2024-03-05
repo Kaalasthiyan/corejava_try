c0b6702951004f9b3da6d4dc1d2ea150381ee3c6
Kaala2402Q <kaalasthiyan100@gmail.com>
2024-03-06 03:36:34 +0530

    try

---
+++ Corejava_newthree/src/trythree/trythree.java
@@ -0,0 +1,13 @@
+package trythree;
+
+
+public class trythree {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=1;
+		b=10;
+		c=a+b;
+		System.out.println("addition of a="+a+" and b="+b+" is :"+c);
+	}
+
+}
